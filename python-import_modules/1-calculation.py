104d29a0435475ee671f2df1503e404cb146a26f
meghan-ke <m.mwizerwa@alustudent.com>
2025-06-26 06:26:59 -0700

    calculator

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mal, div
+a = 10
+b = 5
+def add(a, b):
+    print(a + b)
+def sub(a, b):
+    print(a - b)
+def mal(a, b):
+    print(a * b)
+def div(a, b):
+    print(a / b)
