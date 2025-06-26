98768f5120315abd38693e80fccb55b8e0207bed
meghan-ke <m.mwizerwa@alustudent.com>
2025-06-26 06:31:43 -0700

    calculator

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-from calculator_1.py import add, sub, mal, div
+from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
-def add(a, b):
-    print(a + b)
-def sub(a, b):
-    print(a - b)
-def mal(a, b):
-    print(a * b)
-def div(a, b):
-    print(a / b)
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
