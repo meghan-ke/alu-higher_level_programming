add73a580c7bfc3fdd9e90a996bbfcbdd86a7176
meghan-ke <m.mwizerwa@alustudent.com>
2025-06-27 08:24:47 -0700

    storted dictionary

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """Print dictionary sorted by keys (top level only)."""
+    for key in sorted(a_dictionary):
+        print(f"{key}: {a_dictionary[key]}")
+
