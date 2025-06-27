a6aa12a8621d77d4e3ab7fb6ad2730f25e6f5f38
meghan-ke <m.mwizerwa@alustudent.com>
2025-06-27 08:30:11 -0700

    update dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Add a new key/value pair or update the value if the key exists.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
