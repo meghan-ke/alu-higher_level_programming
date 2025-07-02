13a370f64d4a1f248172857451b33897a94a5f03
meghan-ke <m.mwizerwa@alustudent.com>
2025-07-02 05:48:17 -0700

    count

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list[], x=o):
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
