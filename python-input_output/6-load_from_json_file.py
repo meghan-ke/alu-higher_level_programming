d61775dcb69545601db1f999dc70fd582eb49090
meghan-ke <m.mwizerwa@alustudent.com>
2025-07-18 06:57:24 -0700

    path to json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import json
+
+def load_from_json_file(filename):
+    """
+    Loads a Python object from a JSON file.
+
+    Args:
+        filename (str): The path to the JSON file.
+
+    Returns:
+        object: The Python object represented by the JSON file.
+    """
+    with open(filename, 'r') as f:
+        # Use json.load() to parse the JSON content directly from the file object
+        data = json.load(f)
+    return data
