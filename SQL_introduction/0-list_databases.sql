434d5d07428409e03c19fe32bfc45b5146edf937
meghan-ke <m.mwizerwa@alustudent.com>
2025-07-18 09:26:17 -0700

    all databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-"""
-Script to list all databases in a MySQL server.
-"""
-show databases
+# This script lists all databases of the MySQL server using the MySQLdb module
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect using credentials passed via command line
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2]
+    )
+    cursor = db.cursor()
+    cursor.execute("SHOW DATABASES")
+
+    # Sort and print databases
+    databases = sorted([row[0] for row in cursor])
+    for db_name in databases:
+        print(db_name)
+
+    cursor.close()
+    db.close()
