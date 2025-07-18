325ab580cc7f114f2ad080d60ad6babf26317ac7
meghan-ke <m.mwizerwa@alustudent.com>
2025-07-18 13:06:51 -0700

    foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
-__ a scriot that creates a dataase and a respective table
+--  a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
