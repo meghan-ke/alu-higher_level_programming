2c0a0f0bde4c2d2e1530a74c3d428470abaf4c38
meghan-ke <m.mwizerwa@alustudent.com>
2025-07-25 11:29:03 +0000

    GET request to the URL

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Count and display number of redirections
-echo "$(curl -s -L -o /dev/null -w "%{num_redirects}" "$1") redirection"
+# Script that prints the number of redirections for a given URL
+n=$(curl -s -L -o /dev/null -w "%{num_redirects}" "$1"); [ "$n" -eq 0 ] && echo "no redirection" || echo "$n redirection"
