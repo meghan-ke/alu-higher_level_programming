4fa0e42649af173298cea129e051cd705c4ddc60
meghan-ke <m.mwizerwa@alustudent.com>
2025-07-25 11:10:46 +0000

    Url body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# Script Name: body_size.sh
-# Description: Sends a GET request to a given URL and displays the body size in bytes
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Store the URL
-URL="$1"
-
-# Send request silently (-s) and measure only the body size in bytes
-curl -s "$URL" | wc -c
+# Get response body size in bytes using GET
+curl -s -X GET "$1" | wc -c
