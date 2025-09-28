2d8eed4e4a4da3a18b71c3917de807889ce8ec53
meghan-ke <m.mwizerwa@alustudent.com>
2025-09-28 05:17:09 -0700

    new lines

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+//printing new lines
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
